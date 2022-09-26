3285cdaab4f3c8a6c3f3f930da67a144cd401fda
Dan McAtee <danielmcatee@me.com>
2022-09-26 16:06:47 +0200

    add blog site

---
+++ __posts/second-post.md
@@ -0,0 +1,18 @@
+---
+title: A My second Blog Post
+created: 09-25-2022
+excerpt: This is a excerpt of my second blog post
+tags: ["typescript", "development", "validation"]
+---
+
+# My Second Blog Post
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tincidunt et libero euismod porta. Nulla sit amet augue erat. Fusce accumsan tincidunt fermentum. Phasellus euismod, nisi non ultricies pulvinar, neque sem bibendum orci, a tincidunt elit enim in purus. Pellentesque ac justo velit. Nulla ac dolor ac elit posuere interdum sed in velit. Ut et posuere sapien. Curabitur nec ligula volutpat, tristique enim ac, ultrices nulla.
+
+Donec convallis nunc non nulla facilisis, sed volutpat enim euismod. Ut ac nibh quam. Vestibulum ante lorem, rhoncus eget porta eu, egestas vel risus. Pellentesque ac tristique lectus. Proin molestie ornare est, a vestibulum turpis cursus at. Quisque ac orci eget dui posuere pharetra. Sed lacinia massa vitae molestie tempor. Vivamus eget sollicitudin mauris. Mauris erat risus, volutpat quis egestas nec, vestibulum vel mi. Cras consequat est sollicitudin hendrerit placerat. Mauris accumsan convallis leo, ut vehicula nibh tincidunt eu. Integer pharetra mollis tincidunt. Aenean quis ultricies nunc.
+
+Vivamus scelerisque fringilla justo, vel tincidunt odio consectetur in. Suspendisse mattis imperdiet ipsum luctus commodo. Sed venenatis finibus viverra. Nunc ullamcorper enim eros, quis porta felis faucibus sed. Curabitur vitae diam et lorem pretium sodales. Morbi tempus metus scelerisque imperdiet venenatis. Nullam quis leo sem. Sed laoreet risus at cursus semper. Praesent mollis augue et ornare congue.
+
+Fusce congue tempus semper. Praesent id ullamcorper quam. Quisque ac iaculis eros, luctus placerat tellus. Curabitur at massa sed dolor congue laoreet a nec nibh. Ut sit amet suscipit dolor. Quisque molestie ipsum quis imperdiet egestas. Nulla facilisi. Duis posuere auctor sem sit amet lobortis. Integer pulvinar nisi nec mattis maximus. Mauris tortor justo, ornare vel iaculis a, venenatis nec urna.
+
+Integer consequat magna vitae mauris fringilla, non tincidunt tortor dapibus. Mauris vel risus libero. Curabitur faucibus libero tempus arcu ultrices, vitae lobortis lorem ultrices. Cras ut vehicula orci, in dapibus erat. Proin porta vehicula venenatis. Fusce in porta lorem. Phasellus faucibus in nunc in malesuada. Duis congue leo id neque ornare pretium. Maecenas finibus ornare ullamcorper. Fusce quis ligula placerat, vestibulum tellus at, consequat ex. Proin et aliquet enim, ut tempus est. Pellentesque placerat risus in metus tempus sodales et nec metus.
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,8 +1,101 @@
-export default function Blog() {
+import { GetStaticProps } from "next";
+import fs from "fs/promises";
+import matter from "gray-matter";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+type Post = {
+  title: string;
+  excerpt: string;
+  created: string;
+  tags: string[];
+  slug: string;
+};
+
+interface Props {
+  posts: Post[];
+  tags: string[];
+}
+
+export default function Blog({ posts, tags }: Props) {
+  const [tagFilter, setTagFilter] = useState("");
+  useEffect(() => {
+    console.log(tagFilter);
+  }, [tagFilter]);
   return (
-    <div>
+    <div className="space-y-4">
       <h1>Blog</h1>
-      <p>My blog is gonna be here</p>
+
+      <select
+        onChange={(event) => setTagFilter(event.target.value)}
+        className="border p-2"
+      >
+        <option value="">---</option>
+        {tags.map((tag) => (
+          <option key={tag} value={tag}>
+            {tag}
+          </option>
+        ))}
+      </select>
+
+      <div className="grid grid-cols-2 gap-8">
+        {posts
+          .filter((post) => {
+            if (tagFilter) {
+              return post.tags.includes(tagFilter);
+            }
+            return post;
+          })
+          .sort((a, b) => a.title.localeCompare(b.title))
+          .map((post) => (
+            <Link href={`/blog/${post.slug}`} key={post.slug}>
+              <div className="cursor-pointer aspect-square border p-6 space-y-2">
+                <h2 className="text-2xl font-bold">{post.title}</h2>
+                <p className="text-gray-500 text-sm">{post.created}</p>
+                <div className="flex gap-2">
+                  {post.tags.map((tag) => (
+                    <div
+                      key={tag}
+                      className="py-1 px-2 bg-gray-200 rounded-full"
+                    >
+                      {tag}
+                    </div>
+                  ))}
+                </div>
+                <p>{post.excerpt}</p>
+              </div>
+            </Link>
+          ))}
+      </div>
     </div>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const postsDirectory = await fs.readdir("__posts");
+  let tags: string[] = [];
+  const posts = await Promise.all(
+    postsDirectory.map(async (fileName) => {
+      const file = await fs.readFile(`__posts/${fileName}`, {
+        encoding: "utf-8",
+      });
+      const { data: frontMatter } = matter(file);
+      frontMatter.tags.forEach((tag: string) => {
+        if (!tags.includes(tag)) {
+          tags.push(tag);
+        }
+      });
+      return {
+        ...frontMatter,
+        slug: fileName.replace(".md", ""),
+      };
+    })
+  );
+
+  return {
+    props: {
+      posts,
+      tags: tags.sort(),
+    },
+  };
+};
